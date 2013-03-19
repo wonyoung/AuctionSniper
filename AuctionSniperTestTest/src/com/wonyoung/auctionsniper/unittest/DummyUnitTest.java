eea8f7b0ad51a559c81a82af11d4119fccac0587
wonyoung <wonyoungjjang@gmail.com>
2013-03-19 20:47:35 +0900

    add dummy unit test case

---
+++ AuctionSniperTestTest/src/com/wonyoung/auctionsniper/unittest/DummyUnitTest.java
@@ -0,0 +1,14 @@
+package com.wonyoung.auctionsniper.unittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyUnitTest {
+
+	@Test
+	public void test() {
+		assertTrue("This message cannot be shown", true);
+	}
+
+}
